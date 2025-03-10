091850cbfff312b495101ef6a705276d809c7c48
MSI\hosiu <hosiuchuen05@gmail.com>
2025-03-10 23:14:39 +0800

    test redis starter and yaml file

--- boot3-01-demo/src/main/java/com/schh/boot/controller/HelloController.java
+++ boot3-01-demo/src/main/java/com/schh/boot/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.schh.boot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @GetMapping("/hello")
     public String Hello(){
         return "Hello, Spring Boot3!";
+    }
+
+    @GetMapping("/incr")
+    public String incr(){
+        Long counter = redisTemplate.opsForValue().increment("counter", 1);
+        return "counter: " + counter;
     }
 }
